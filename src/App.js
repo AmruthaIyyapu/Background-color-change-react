623a829d2d5a5bf7b334eba571e65626d05488d6
AmruthaIyyapu <97259179+AmruthaIyyapu@users.noreply.github.com>
2023-03-28 12:03:18 -0400

    Added the change color function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 
 export default function App() {
+  function changeColor(event) {
+    document.body.style.background = event.target.value;
+  }
+
   return (
     <div className="App">
-      <h1>Background color change</h1>
-    <input type="color" />
-      
+      <h1>Background colour change</h1>
+      <input type="color" onChange={changeColor} />
     </div>
   );
 }
